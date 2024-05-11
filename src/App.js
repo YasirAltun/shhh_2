271d22f5f92412d3a906e8ffcdabfe3e65fb6aa3
YasirAltun <ayasiraltun@gmail.com>
2024-05-11 17:42:39 +0300

    container lı hale getirildi

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import './App.css';
 import StackCard from './Components/StackCard';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Navi from "./navbar/Navi";
+import { Container, Row, Col } from 'reactstrap';
+
 
 
 function App() {
   return (
-    <div className="App">
-       {/* navbar */}
-       <div className='NAVI'>
-        <Navi />
-      </div>
-      <Routes>
-        <Route path="/" element={<StackCard />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
+
+    <div>
+      <Container>
+        <Row>
+          <Navi/>
+        </Row>
+        <Row>
+          <Col xs="3">  
+            {/*Category list */}
+          </Col>
+          <Col xs="9">  
+           {/*card list */}
+            <Routes>
+              <Route path="/" element={<StackCard />} />
+            </Routes>
+          </Col>
+
+
+        </Row>
+      </Container>
+  </div>
+
+
   );
 }
 
